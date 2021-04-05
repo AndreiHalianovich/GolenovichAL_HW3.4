5631df06a82321d49e35fa4224cdf60573b975aa
Andrei Halianovich <igolenovichbox@gmail.com>
2021-04-05 20:19:21 +0300

    Initial Commit

---
+++ GolenovichAL_HW3.4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GolenovichAL_HW3.4
+//
+//  Created by Andrei Halianovich on 5.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
